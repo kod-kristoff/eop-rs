fba84872a33995eea6253621116000c955ac88bf
Kristoffer Andersson <kod.kristoff@gmail.com>
2025-05-11 22:25:01 +0200

    feat: add power_right_associated

--- src/math/power_binary.rs
+++ src/math/power_binary.rs
@@ -13,3 +13,17 @@ where
     }
     op.call(power_left_associated(a.clone(), n.predecessor(), op), a)
 }
+
+/// Precondition: `n > 0`
+pub fn power_right_associated<I, Op, Args>(a: Op::Domain, n: I, op: Op) -> Op::Domain
+where
+    I: Integer,
+    Op: BinaryOperation<Args> + Copy,
+    Op::Domain: Clone,
+{
+    // precondition: n > 0
+    if n == I::one() {
+        return a;
+    }
+    op.call(a.clone(), power_right_associated(a, n.predecessor(), op))
+}
--- tests/api_tests/chapter_03.rs
+++ tests/api_tests/chapter_03.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Mul};
+use std::ops::{Add, Mul, Sub};
 
 use eop::math::power_binary::power_left_associated;
 
@@ -7,6 +7,13 @@ where
     T: Add<Output = T>,
 {
     x + y
+}
+
+fn minus<T>(x: T, y: T) -> T
+where
+    T: Sub<Output = T>,
+{
+    x - y
 }
 
 fn multiply<T>(x: T, y: T) -> T
@@ -18,6 +25,15 @@ where
 
 fn concat(x: String, y: String) -> String {
     format!("{x}{y}")
+}
+
+fn algorithm_power(pow: fn(i32, i32, fn(i32, i32) -> i32) -> i32) {
+    assert_eq!(pow(1, 1, multiply), 1);
+    assert_eq!(pow(10, 1, multiply), 10);
+    assert_eq!(pow(1, 10, multiply), 1);
+    assert_eq!(pow(2, 2, multiply), 4);
+    assert_eq!(pow(2, 10, multiply), 1024);
+    assert_eq!(pow(10, 2, multiply), 100);
 }
 
 mod power_left_associated {
@@ -35,29 +51,39 @@ mod power_left_associated {
         assert_eq!(&power_left_associated(a, 3, concat), "odeodeode");
     }
     #[test]
-    fn works_with_plus() {
-        let a = 5;
-        assert_eq!(power_left_associated(a, 5, plus), 25);
+    fn works() {
+        assert_eq!(power_left_associated(-2, 3, minus), 2);
+        assert_eq!(power_left_associated(-2, 4, minus), 4);
+        algorithm_power(power_left_associated);
     }
 }
 
 mod power_right_associated {
+    use eop::math::power_binary::power_right_associated;
+
     use super::*;
 
     #[test]
     fn returns_same_when_n_is_1() {
         let a = 5;
-        assert_eq!(power_left_associated(a, 1, plus), a);
+        assert_eq!(power_right_associated(a, 1, plus), a);
     }
 
     #[test]
     fn works_with_concat() {
         let a = "ode".to_string();
-        assert_eq!(&power_left_associated(a, 3, concat), "odeodeode");
+        assert_eq!(&power_right_associated(a, 3, concat), "odeodeode");
     }
     #[test]
     fn works_with_multiply() {
         let a = 5;
-        assert_eq!(power_left_associated(a, 5, plus), 25);
+        assert_eq!(power_right_associated(a, 5, multiply), 3125);
+    }
+
+    #[test]
+    fn works() {
+        assert_eq!(power_right_associated(-2, 3, minus), -2);
+        assert_eq!(power_right_associated(-2, 4, minus), 0);
+        algorithm_power(power_right_associated);
     }
 }
